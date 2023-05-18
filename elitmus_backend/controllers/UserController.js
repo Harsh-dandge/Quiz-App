8ce7c2461436124ea1c1245d0af12d8d0859960c
harsh_dandge <harshvardhan.dandge@gmail.com>
2023-05-18 18:00:36 +0530

    Refactored try catch

--- elitmus_backend/controllers/UserController.js
+++ elitmus_backend/controllers/UserController.js
@@ -5,29 +5,34 @@ const {sign, verify} = pkg;
 const  SECRET_KEY = "NOTESAPI";
 
 const signup = async( req, res) => {
-    const {name, email, password} = req.body;
-    
-    console.log(req.body);
-// existing user check 
-    let user = await User.findOne({where: {
-        email
-      }});
+    try{
+        const {name, email, password} = req.body;
+        
+        console.log(req.body);
+    // existing user check 
+        let user = await User.findOne({where: {
+            email
+        }});
 
-    if(user){
-        return res.status(400).json({error:"User Already Exists"});
+        if(user){
+            return res.status(400).json({error:"User Already Exists"});
+        }
+    // used argon instead of bcrypt , new way of hashing 
+        let passwordHash = await argon2.hash(password);
+
+        // user creation 
+        user = await User.create({
+            name,
+            email,
+            password: passwordHash
+        });
+
+        const token = sign({email,id : user.userId},SECRET_KEY)
+        return res.status(201).json({user,token});
     }
-// used argon instead of bcrypt , new way of hashing 
-    let passwordHash = await argon2.hash(password);
-
-    // user creation 
-    user = await User.create({
-        name,
-        email,
-        password: passwordHash
-    });
-
-    const token = sign({email,id : user.userId},SECRET_KEY)
-    return res.status(201).json({user,token});
+    catch(error){
+            res.status(400).json({error:`Bad Request ${error}`});
+    }
 }
 
 
